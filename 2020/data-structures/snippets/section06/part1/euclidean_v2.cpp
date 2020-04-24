3b067215e77e86852a7557723dce320722c8fd3b
Asem Alaa <asem_alla@msn.com>
2020-04-24 05:38:28 +0200

    add section 6 snippets

---
+++ 2020/data-structures/snippets/section06/part1/euclidean_v2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+
+struct Point
+{
+    // Function members (methods)
+    double distance( Point other )
+    {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return std::sqrt( dx * dx + dy * dy );
+    }
+    // Default constructor.
+    Point()
+    {
+        x = 0;
+        y = 0;
+    }
+
+    // Data memebers
+    double x;
+    double y;
+};
+
+int main()
+{
+    Point p, q; //all members initialized to zero.
+    std::cout << "Enter the two points coordinates as following: x1 y1 x2 y2 [ENTER]\n";
+    std::cin >> p.x >> p.y >> q.x >> q.y;
+    std::cout << p.distance( q ) << "\n";
+
+    // Note that: p.distance( q ) == q.distance( p )
+}
